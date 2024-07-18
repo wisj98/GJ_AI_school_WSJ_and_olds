dae6ab595fe532fdd00d611c81a83e73042d8690
wisj98 <wisj98@naver.com>
2024-07-18 13:54:48 +0900

    updatae

--- train_to_metadata.py
+++ train_to_metadata.py
@@ -6,6 +6,7 @@ folder_list = os.listdir("images")
 file_list = []
 meme_class = []
 for i in folder_list:
+    if len(i.split('.')) != 1: continue
     file_list += list(map(lambda x: f"images/{i}/{x}",os.listdir(f"images/{i}")))
 print(file_list)
 
@@ -23,7 +24,7 @@ print(train_data.head())
 sizes = train_data["Path"].apply(lambda x: Image.open(x).size)
 train_data["h"] = sizes.apply(lambda x: x[0])
 train_data["w"] = sizes.apply(lambda x: x[1])
-train_data["Class"] = train_data["Path"].apply(lambda x: int(x.split('/')[1][4:]))
+train_data["Class"] = train_data["Path"].apply(lambda x: (x.split('/')[1]))
 
 print(train_data)
 
